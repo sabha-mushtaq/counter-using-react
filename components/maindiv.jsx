bd1580f82f81a0f70ef422d33679f99c48a7a48b
sabha mushtaq <sabhamushtaq191@gmail.com>
2025-01-06 21:27:14 +0530

    counter using react

--- components/maindiv.jsx
+++ components/maindiv.jsx
@@ -1,54 +1,34 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import './maindiv.css';
 
 function Container() {
-    // Create a state variable to toggle between Sign In and Sign Up forms
-    const [isSignUp, setIsSignUp] = useState(false);
+  const [count, setCount] = useState(0);
 
-    // Function to toggle between Sign In and Sign Up
-    const toggleSignUp = () => {
-        setIsSignUp(!isSignUp);
-    };
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-    return (
-        <> 
-            <div className="con">
-                <div className="logo">
-                <img src='/inst.png' alt="logo" className="logo-img" />
-                </div>
-                <div className="input-fields">
-                    {/* Common inputs for both Sign In and Sign Up */}
-                    <input placeholder="Phone number, Username, or Email" />
-                    <input placeholder="Password" />
-                    
-                    {/* Conditionally render Full Name and Bio for Sign Up */}
-                    {isSignUp && (
-                        <>
-                            <input placeholder="Full Name" />
-                            <input placeholder="Bio" />
-                        </>
-                    )}
-                </div>
-                <div className="button">
-                    <button className="w-full py-2 bg-blue-500 text-white">
-                        {isSignUp ? 'Sign Up' : 'Sign In'}
-                    </button>
-                    {/* Toggle between Sign In and Sign Up */}
-                    <p>
-                        {isSignUp ? (
-                            <span onClick={toggleSignUp}>Already have an account? Sign In</span>
-                        ) : (
-                            <span onClick={toggleSignUp}>Don&apos;t have an account? Sign Up</span>
-                        )}
-                    </p>
-                </div>
-            </div>
-        </>
-    );
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <> 
+      <div className="container">
+        <div className='count-container'>
+          <div className='count'>
+            {count}
+          </div>
+        </div>
+        <div className='buttons-container'>
+          <button className='increment' onClick={increment}>Increment</button>
+          <button className='decrement' onClick={decrement}>Decrement</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Container;
-
-
 
 
